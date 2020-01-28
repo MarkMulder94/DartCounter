820d61f6a025236e1a3f605a22b4919134df73e0
MarkMulder94 <mark_mulder@hotmail.nl>
2020-01-28 13:27:49 +0100

    Adding Throw, unittested

---
+++ Darts.Lib/Game/GameRunner.cs
@@ -0,0 +1,62 @@
+﻿
+
+using Darts.Lib.DataAcces;
+using Darts.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Darts.Lib.Game
+{
+    public class GameRunner
+    {
+        private readonly int _gameId;
+        private List<TeamModel> _teams;
+        private List<WantGamePlayerModel> _competitors;
+
+
+        public GameRunner(int gameId)
+        {
+            _gameId = gameId;
+            _teams = GetTeams.GetTheTeamsOnExistingGame(_gameId);
+        }
+
+        public void ThrowTurn(int score)
+        {
+            int teamTurn = 0;
+            RegisterThrow(teamTurn, score);
+            if (teamTurn == 0)
+            {
+                teamTurn++;
+            }
+            else { teamTurn--; }
+        }
+        public void RegisterThrow(int teamTurn, int thrownScore)
+        {
+            switch (teamTurn)
+            {
+                case 0:
+                    _teams[0].CurrentScore -= thrownScore;
+                    break;
+                case 1:
+                    _teams[1].CurrentScore -= thrownScore;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public List<int> Scores()
+        {
+            List<int> scores = new List<int>();
+            foreach (var team in _teams)
+            {
+                scores.Add(team.CurrentScore);
+            }
+            return scores;
+        }
+    }
+}
--- UnitTests/Core/Game/GameRunnerTests/GameLoopTests.cs
+++ UnitTests/Core/Game/GameRunnerTests/GameLoopTests.cs
@@ -14,7 +14,7 @@ namespace UnitTests.Core.Game.GameRunnerTests
         {
             var playerList = new List<PlayerModel>();
 
-            var player1 = new PlayerModel("Mark", "Mulder") { Player_Id= 1};
+            var player1 = new PlayerModel("Mark", "Mulder") { Player_Id = 1 };
             var player2 = new PlayerModel("Kelly", "Schinkel") { Player_Id = 2 };
             var player3 = new PlayerModel("Iemand", "Mulder") { Player_Id = 3 };
             var player4 = new PlayerModel("Test", "Mulder") { Player_Id = 4 };
@@ -31,8 +31,8 @@ namespace UnitTests.Core.Game.GameRunnerTests
             var userList = SeedUsers();
             var playerList = new List<WantGamePlayerModel>();
 
-            var player1 = new WantGamePlayerModel(userList.FirstOrDefault(x => x.Player_Id == idPlayer1),1, idPlayer1);
-            var player2 = new WantGamePlayerModel(userList.FirstOrDefault(x => x.Player_Id == idPlayer2),2, idPlayer2);
+            var player1 = new WantGamePlayerModel(userList.FirstOrDefault(x => x.Player_Id == idPlayer1), 1, idPlayer1);
+            var player2 = new WantGamePlayerModel(userList.FirstOrDefault(x => x.Player_Id == idPlayer2), 2, idPlayer2);
 
             playerList.Add(player1);
             playerList.Add(player2);
@@ -52,16 +52,19 @@ namespace UnitTests.Core.Game.GameRunnerTests
             var result = GetTeams.GetTheTeamsOnExistingGame(5);
             result.Should().HaveCount(2);
         }
-        [Fact]
-        public void GiveBackTeamds_GivenGameId()
+        [Theory]
+        [InlineData(20, 481)]
+        public void GiveBackNewScore_GivenCurrentThrow(int score, int expected)
         {
-            var result = new MakeGame(5);
-            var test = result.StartTheGame();
+            List<int> StartScores = new List<int>() { 501, 501 };
+            var game = new GameRunner(5);
+            var ScoresBeforeThrow = game.Scores();
+            game.ThrowTurn(score);
+            var ScoresAfterThrow = game.Scores();
 
-
-            test.Should().Be(1);
+            Assert.Equal(StartScores, ScoresBeforeThrow);
+            Assert.Equal(expected, ScoresAfterThrow[0]);
         }
-
 
 
 
